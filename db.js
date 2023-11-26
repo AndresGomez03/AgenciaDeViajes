9b6abe23f126a0bd2edb5020603c5a9644928ac2
gerardo <garaneda@utem.cl>
2023-11-26 15:27:12 -0300

    agregando .gitignore con mi usuario en db.js y node_modules

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
     host: 'localhost',
-    user: 'root',
-    password: '123456',
+    user: 'gerardoDBA',
+    password: '0451',
     port: 3306,
     database: 'agenciaviajes'
 })
